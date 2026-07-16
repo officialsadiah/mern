00260d8e900f7e964c4e00649a11219f9930c0a5
officialsadiah <sadia.siddiqie33@gmail.com>
2026-07-16 14:04:29 +0600

    dom clas

--- script31.js
+++ script31.js
@@ -1,3 +1,22 @@
-let heading = document.getElementsByTagName('h1')[0]
 
-heading.textContent="bangladesh"
+
+
+let heading = document.getElementsByClassName("x")
+
+heading[0].innerHTML="sad"
+// we need to use [0] so it knows which element with "getElementsByClassName"
+
+let heading = document.querySelector(".x")
+heading.innerHTML="sadia"
+
+
+
+
+// document is like a book, inside it all the chapters
+// getElementsByTagName('h1')[0] = first element of h1 
+// innerHTML = inside html like <h1>hello</h1> , so hello is inside html h1
+// if <h1><b>hello</b></h1> , the inner html will be inside b
+// = "sadiah" replace the title with "sadiah"
+// "Go to the webpage → find all <h1> elements → 
+// choose the first one → replace its content with sadiah."
+
