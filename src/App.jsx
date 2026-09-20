3552b282370575e47adafad6bf2ac30bfd6059e0
officialsadiah <sadia.siddiqie33@gmail.com>
2026-09-20 16:28:16 +0200

    components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Heading from './components/Heading'
+const App = () => {
+  return (
+    <>
+      <Heading text="AAAA" description="aaaaaaaaaaaaaaaaaaaaaaaa"/>
+       
+      
+    </>
+  )
+}
+
+export default App
