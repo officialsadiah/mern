c5839c52f66efdac9b67149492031453dcfd89f3
officialsadiah <sadia.siddiqie33@gmail.com>
2026-09-20 18:24:36 +0200

    class 42 card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import Heading from './components/Heading'
+import Card from './components/Card'
+// import Heading from './components/Heading'
 const App = () => {
   return (
     <>
-      <Heading text="AAAA" description="aaaaaaaaaaaaaaaaaaaaaaaa"/>
-       
-      
+   <Card type={false} design="main" Title="Card One" Paragraph="lorem21" Botton="click"  />       
+   <Card type={true} design="main2" Title="Card Two" Paragraph="lorem21" Botton="dont click"  />       
+      {/* true and false must be in { }  */}
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,15 @@
 import React from "react"
  
-const Card = () => {
-
+const Card = ({Title,Paragraph,Botton,design,type}) => {
+return(
+    <div className={design}>
+    <h1>{Title}</h1>
+    <p>{Paragraph}</p>
+    <button className={type? "button": "button2"}>{Botton}</button>
+    {/* if the type is true, it will use {button} design, if false {button2} design
+    you have to write something in botton so it shows written things */}
+    </div>
+)
 }
 
 export default Card
